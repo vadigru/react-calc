ec29e00a297b764d09cf7821e4329be6c2f6ad73
vadigru <vg@outlook.ie>
2021-02-04 15:55:51 +0200

    fixes indicators font color

--- src/component/display/display.js
+++ src/component/display/display.js
@@ -23,12 +23,16 @@ const Display = (props) => {
     backgroundColor: `${displayColor}`
   };
 
+  const styleFontColor = {
+    color: `${displayColor}`
+  };
+
   return (
     <div className={`display display${isCalcOn ? `--on` : `--off`}`} style={styleDisplayColor}>
       <ul className={`display__indicators indicators`}>
-        <li className={`indicators__item indicators__res indicators__item${isResult ? `--on` : `--off`}`}>RES</li>
-        <li className={`indicators__item indicators__sqrt indicators__item${isSqrtResult ? `--on` : `--off`}`}>SQRT</li>
-        <li className={`indicators__item indicators__err indicators__item${isError ? `--err-on` : `--err-off`}`}>ERR</li>
+        <li className={`indicators__item indicators__res indicators__item${isResult ? `--on` : `--off`}`} style={styleFontColor}>RES</li>
+        <li className={`indicators__item indicators__sqrt indicators__item${isSqrtResult ? `--on` : `--off`}`} style={styleFontColor}>SQRT</li>
+        <li className={`indicators__item indicators__err indicators__item${isError ? `--err-on` : `--err-off`}`} style={styleFontColor}>ERR</li>
         <li className={`indicators__item indicators__total indicators__item${isCalcOn ? `--total-on` : `--total-off`}`}>{total}</li>
       </ul>
 
